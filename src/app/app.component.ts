00581670935872b0359b95a05d70f0cdd7717262
titouan-aclr <122918576+titouan-aclr@users.noreply.github.com>
2023-12-12 10:39:35 +0100

    sous chapitre 3.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,31 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
   
   ngOnInit(): void {
-      this.mySnap = {
+      this.faceSnaps = [{
         title: 'Archibald',
         description: 'Mon meilleur ami depuis tout petit !',
         createdDate: new Date(),
         snaps: 0,
         imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
         location: 'Paris'
-      };
-      this.myOtherSnap = {
+      },
+      {
         title: 'Three Rock Mountain',
         description: 'Un endroit magnifique pour les randonnées',
         createdDate: new Date(),
         snaps: 0,
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/280px-Three_Rock_Mountain_Southern_Tor.jpg',
         location: 'la montagne'
-      };
-      this.myLastSnap = {
+      },
+      {
         title: 'Un bon repas',
         description: 'Mmmh que c\'est bon!',
         createdDate: new Date(),
         snaps: 0,
         imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg'
-      }
+      }]
   }
 }
