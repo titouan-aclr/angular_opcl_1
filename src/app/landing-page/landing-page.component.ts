b8079b3bfc4b11f58a23a968deb98fce01ad85f7
Titouan Aclr <122918576+titouan-aclr@users.noreply.github.com>
2024-09-25 17:40:37 +0200

    sous chapitre 5.4 - navigate between routes

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  constructor(private router: Router) { }
 
+  onContinue() {
+    this.router.navigateByUrl('facesnaps');
+  }
 }
