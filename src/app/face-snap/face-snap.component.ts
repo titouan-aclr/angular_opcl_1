06a8055317e1b986b867c5fa3d6703a798a74202
Titouan Aclr <122918576+titouan-aclr@users.noreply.github.com>
2024-09-25 14:33:53 +0200

    sous chapitre 4.2 - date pipe & local config

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,11 +1,11 @@
-import { NgClass, NgStyle, UpperCasePipe } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [NgClass, NgStyle, UpperCasePipe],
+  imports: [NgClass, NgStyle, UpperCasePipe, DatePipe],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss',
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+registerLocaleData(fr.default);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
