dd50b6876ccdae68e090ced67e7faf99baa4fc88
Titouan Aclr <122918576+titouan-aclr@users.noreply.github.com>
2024-09-25 16:04:29 +0200

    sous chapitre 5.1 - add faceSnapsService

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -12,31 +13,9 @@ import { FaceSnap } from '../models/face-snap.model';
 export class FaceSnapListComponent implements OnInit {
   faceSnaps!: FaceSnap[];
 
+  constructor(private faceSnapsService: FaceSnapsService) { }
+
   ngOnInit(): void {
-    this.faceSnaps = [
-      new FaceSnap(
-        'Archibald',
-        'Mon meilleur ami depuis tout petit !',
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        new Date(),
-        160,
-        'Paris'
-      ),
-      new FaceSnap(
-        'Three Rock Mountain',
-        'Un endroit magnifique pour les randonnées',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/280px-Three_Rock_Mountain_Southern_Tor.jpg',
-        new Date(),
-        30,
-        'la montagne'
-      ),
-      new FaceSnap(
-        'Un bon repas',
-        "Mmmh que c'est bon!",
-        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        new Date(),
-        0
-      ),
-    ];
+    this.faceSnaps = this.faceSnapsService.getFaceSnaps();
   }
 }
