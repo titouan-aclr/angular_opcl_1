1e078456aaee23df9b1349183410d99bf48af1e3
Titouan Aclr <122918576+titouan-aclr@users.noreply.github.com>
2024-11-15 16:30:03 +0100

    C2 - sous chapitre 2.1

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+
+  userEmail!: string;
+
   constructor(private router: Router) { }
 
   onContinue() {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm): void {
+    console.log(form.value);
   }
 }
